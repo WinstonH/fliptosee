b234219d0c5593c762c645e934167775ef083d41
WinstonH <bjdhjj@126.com>
2017-04-17 20:38:08 +0800

    Update onestep.sh

--- onestep.sh
+++ onestep.sh
@@ -14,34 +14,9 @@ tar -zxvf kcptun-linux-amd64-20161222.tar.gz
 rm /root/kcptun/start.sh
 rm /root/kcptun/stop.sh
 rm /root/kcptun/restart.sh
-wget --no-check-certificate https://github.com/WinstonH/fliptosee/raw/master/order.zip
-unzip order.zip
-rm /root/kcptun/order.zip
-# 创建Kcptun配置
-touch /root/kcptun/server-config.json
-echo "" > /root/kcptun/server-config.json
-echo "{
-    "listen": ":4000",
-    "target": "127.0.0.1:443",
-    "key": "kcptest",
-    "crypt": "aes-192",
-    "mode": "fast2",
-    "mtu": 1400,
-    "sndwnd": 512,
-    "rcvwnd": 512,
-    "datashard": 70,
-    "parityshard": 30,
-    "dscp": 46,
-    "nocomp": false,
-    "acknodelay": false,
-    "nodelay": 0,
-    "interval": 40,
-    "resend": 0,
-    "nc": 0,
-    "sockbuf": 4194304,
-    "keepalive": 10,
-    "log": "/root/kcptun/kcptun.log"
-}" >> /root/kcptun/server-config.json
+wget --no-check-certificate https://github.com/WinstonH/fliptosee/raw/master/packup.zip
+unzip packup.zip
+rm /root/kcptun/packup.zip
 chmod +x /root/kcptun/*.sh
 sh /root/kcptun/start.sh
 chmod +x /etc/rc.d/rc.local && echo "sh /root/kcptun/start.sh" >> /etc/rc.d/rc.local
