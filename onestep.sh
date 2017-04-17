f1d0489a23ea63e67b5b632b323c258e97cc67eb
WinstonH <bjdhjj@126.com>
2017-04-17 19:41:42 +0800

    Update onestep.sh

--- onestep.sh
+++ onestep.sh
@@ -4,19 +4,16 @@ yum -y install git
 git clone -b manyuser https://github.com/shadowsocksr/shadowsocksr.git
 cd /root/shadowsocksr/shadowsocks
 python server.py -p 443 -k kcpbeta -m aes-256-cfb -O origin -o plain -d start
-# Now install Kcptun
 mkdir /root/kcptun
 cd /root/kcptun
 wget https://github.com/xtaci/kcptun/releases/download/v20161222/kcptun-linux-amd64-20161222.tar.gz
 tar -zxvf kcptun-linux-amd64-20161222.tar.gz
-# 创建 start.sh
 touch /root/kcptun/start.sh
 echo "" > /root/kcptun/start.sh
 echo "
 cd /root/kcptun/
 ./server_linux_amd64 -c /root/kcptun/server-config.json 2>&1 &
 echo "Kcptun started."" >> /root/kcptun/start.sh
-# 创建Kcptun配置
 touch /root/kcptun/server-config.json
 echo "" > /root/kcptun/server-config.json
 echo "{
@@ -41,7 +38,6 @@ echo "{
     "keepalive": 10,
     "log": "/root/kcptun/kcptun.log"
 }" >> /root/kcptun/server-config.json
-# 创建 stop.sh
 touch /root/kcptun/stop.sh
 echo "" > /root/kcptun/stop.sh
 echo "PID=`ps -ef | grep server_linux_amd64 | grep -v grep | awk '{print $2}'`
@@ -49,7 +45,6 @@ if [ "" != "$PID" ]; then
  echo "killing $PID"
  kill -9 $PID
 fi" >> /root/kcptun/stop.sh
-# 创建 restart.sh
 touch /root/kcptun/stop.sh
 echo "" > /root/kcptun/restart.sh
 echo "cd /root/kcptun/
@@ -57,7 +52,6 @@ echo "Stopping Kcptun..."
 bash stop.sh
 bash start.sh
 echo "Kcptun started."" >> /root/kcptun/restart.sh
-
 chmod +x /root/kcptun/*.sh
 sh /root/kcptun/start.sh
 chmod +x /etc/rc.d/rc.local && echo "sh /root/kcptun/start.sh" >> /etc/rc.d/rc.local
