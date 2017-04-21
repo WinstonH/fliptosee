bf6bfdb9139b0581913cb9b09a8a0f282ea83316
WinstonH <bjdhjj@126.com>
2017-04-21 14:53:26 +0800

    Update autologin.sh

--- autologin.sh
+++ autologin.sh
@@ -2,13 +2,13 @@
  passwd='root'
  /usr/bin/expect <<-EOF
  set time 30
-spawn ssh  -p 22 root@192.168.1.1
+spawn ssh -p 22 root@192.168.1.1
  expect {
  "*yes/no" { send "yes\r"; exp_continue }
  "*password:" { send "$passwd\r" }
  }
  expect "*#"
- send "sh dev.sh\r"
+ send "sh onestep_ubuntu.sh\r"
  expect "*#"
  send "uptime\r"
  expect "*#"
