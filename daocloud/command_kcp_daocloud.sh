5aab5fadbbf05420f83644eda5961a89a9a3b0c1
WinstonH <bjdhjj@126.com>
2017-04-21 16:32:04 +0800

    Update command_kcp_daocloud.sh

--- daocloud/command_kcp_daocloud.sh
+++ daocloud/command_kcp_daocloud.sh
@@ -2,18 +2,17 @@
 # step1 get the ip address
 sh get_arukas_ip.sh
 IP=`cat /root/sakura_ip.txt`
-# step2 get the ssh port
-sh get_arukas_ssh_port.sh
-PORT=`cat /root/sakura_ssh_port.txt`
+# step2 get the kcp port
+sh get_arukas_kcp_port.sh
+PORT=`cat /root/sakura_kcp_port.txt`
 # step3 creat a new command line
-COM="spawn ssh -p "${PORT}" root@"${IP}
+COM=${IP}":"${PORT}
 echo "$COM" >> command.txt
 # step4 reset all var
-rm /root/sakura_ip.txt /root/sakura_ssh_port.txt
+rm /root/sakura_ip.txt /root/sakura_kcp_port.txt
 unset IP PORT COM
 cat command.txt
 # step5 replace the command
-sed -i '5d' /root/autologin_arukas.sh
-sed -i "5 s/^/$(echo $(cat command.txt))\n/" /root/autologin_arukas.sh
+sed -i 's/10.10.10.10:4000/$(echo $(cat command.txt))/g' /root/kcptun/client-config.json
 rm  command.txt
-sh autologin_arukas.sh
+sh /root/kcptun/start.sh
