91c47d7494a8a476f7ef576c03c3cfba74e9bc58
WinstonH <bjdhjj@126.com>
2017-04-21 15:53:53 +0800

    Update get_arukas_ssh_port.sh

--- get_arukas_ssh_port.sh
+++ get_arukas_ssh_port.sh
@@ -2,11 +2,11 @@
 curl -n https://app.arukas.io/api/containers \
   -H "Content-Type: application/vnd.api+json"  \
   -H "Accept: application/vnd.api+json" >> /root/sakura_temp.txt
-cat /root/sakura_temp.txt | awk -F '19606bff-3875-4d7d-b253-f755d1863ddc' ' {print $2}' >> /root/sakura.txt
+cat /root/sakura_temp.txt | awk -F '382146ff-e06a-49ee-b9f9-5833cf7048e1' ' {print $2}' >> /root/sakura.txt
 rm /root/sakura_temp.txt
 cat /root/sakura.txt | awk -F 'service_port":' ' {print $2}' >> /root/sakura_temp.txt
 rm /root/sakura.txt
-cat /root/sakura_temp.txt | awk -F ',' ' {print $1}' >> /root/sakura_port.txt
+cat /root/sakura_temp.txt | awk -F ',' ' {print $1}' >> /root/sakura_ssh_port.txt
 rm /root/sakura_temp.txt
-cat /root/sakura_port.txt
-echo "Successful get the port!"
+cat /root/sakura_ssh_port.txt
+echo "Successful get the ssh port!"
