d2d13f92243006bf555736a4d978be350eb1bb8c
WinstonH <bjdhjj@126.com>
2017-04-21 16:34:55 +0800

    Update command_kcp_daocloud.sh

--- daocloud/command_kcp_daocloud.sh
+++ daocloud/command_kcp_daocloud.sh
@@ -7,12 +7,11 @@ sh get_arukas_kcp_port.sh
 PORT=`cat /root/sakura_kcp_port.txt`
 # step3 creat a new command line
 COM=${IP}":"${PORT}
-echo "$COM" >> command.txt
+echo "$COM"
 # step4 reset all var
 rm /root/sakura_ip.txt /root/sakura_kcp_port.txt
-unset IP PORT COM
-cat command.txt
+unset IP PORT
 # step5 replace the command
-sed -i 's/10.10.10.10:4000/$(echo $(cat command.txt))/g' /root/kcptun/client-config.json
-rm  command.txt
+sed -i "s/10.10.10.10:4000/$COM/g" /root/kcptun/client-config.json
+unset COM
 sh /root/kcptun/start.sh
