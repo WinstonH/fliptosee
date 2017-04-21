2efc906c1d7757f6b2147262b62d0b08557db8e9
WinstonH <bjdhjj@126.com>
2017-04-21 15:55:29 +0800

    Update command_ssh_arukas.sh

--- command_ssh_arukas.sh
+++ command_ssh_arukas.sh
@@ -3,13 +3,13 @@
 sh get_arukas_ip.sh
 IP=`cat /root/sakura_ip.txt`
 # step2 get the ssh port
-sh get_arukas_port.sh
-PORT=`cat /root/sakura_port.txt`
+sh get_arukas_ssh_port.sh
+PORT=`cat /root/sakura_ssh_port.txt`
 # step3 creat a new command line
 COM="spawn ssh -p "${PORT}" root@"${IP}
 echo "$COM" >> command.txt
 # step4 reset all var
-rm /root/sakura_ip.txt /root/sakura_port.txt
+rm /root/sakura_ip.txt /root/sakura_ssh_port.txt
 unset IP PORT COM
 cat command.txt
 # step5 replace the command
