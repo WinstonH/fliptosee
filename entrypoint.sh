725fe46271f4e4f4e0f055e13a090895f98f084a
WinstonH <bjdhjj@126.com>
2017-05-31 22:10:52 +0800

    Update and rename hook.sh to entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# ocdownloader requirements
+su - aria2 -c '/usr/bin/aria2c --enable-rpc --on-download-complete=/hook.sh --rpc-allow-origin-all -c -D --log=/dev/stdout --check-certificate=false &'
+
+# makes aria2 and www-data works together
+umask 0007
+apache2-foreground
